1945a0030f7bf86dd9fca46cc9858c78334d8a93
Anushk Vijayvargiya <anushk.vijayvargiya.17cse@bml.edu.in>
2021-01-14 17:29:57 +0530

    Added API

--- config/eslint.config.js
+++ config/eslint.config.js
@@ -19,6 +19,7 @@ module.exports = {
 
   parserOptions: {
     ecmaVersion: 9,
+    "sourceType": "module",
     ecmaFeatures: {
       jsx: true,
       modules: true,
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,7 +2,7 @@ import React, {  useEffect } from 'react';
 import { Helmet } from 'react-helmet';
 import L from 'leaflet';
 
-
+import axios from 'axios';
 import Layout from 'components/Layout';
 import Container from 'components/Container';
 import Map from 'components/Map';
@@ -24,7 +24,14 @@ const DEFAULT_ZOOM = 2;
  */
 
 const MapEffect = ({ leafletElement: map }) => {
-  
+  let response;
+  try {
+    response = await axios.get('https://corona.lmao.ninja/v2/countries');
+  } catch(e){
+    console.log(`Failed to fetch countries: ${e.message}`, e);
+    return;
+  }
+  const { data =[] } = response;
 };
 
 const IndexPage = () => {
